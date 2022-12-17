ef8640461581ad4f6de2914a7715ca6d1178fd8a
liaoliao666 <1076988944@qq.com>
2022-12-18 01:18:38 +0800

    feat: update build number

--- types.tsx
+++ types.tsx
@@ -2,7 +2,6 @@
  * Learn more about using TypeScript with React Navigation:
  * https://reactnavigation.org/docs/typescript/
  */
-import { NavigatorScreenParams } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { z } from 'zod'
 
@@ -16,7 +15,6 @@ declare global {
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined
   SortTabs: undefined
   NotFound: undefined
   MyNodes: undefined
