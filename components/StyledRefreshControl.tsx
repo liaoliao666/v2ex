b97e30a49c1b880e9602accf511f2647c539e758
liaoliao666 <1076988944@qq.com>
2024-11-27 21:27:08 +0800

    chore: upgrade version

--- components/StyledRefreshControl.tsx
+++ components/StyledRefreshControl.tsx
@@ -2,12 +2,22 @@ import { useAtomValue } from 'jotai'
 import { RefreshControl, RefreshControlProps } from 'react-native'
 
 import { colorSchemeAtom } from '@/jotai/themeAtom'
+import { uiAtom } from '@/jotai/uiAtom'
 
 export default function StyledRefreshControl({
   tintColor,
   ...props
 }: RefreshControlProps) {
+  const ui = useAtomValue(uiAtom)
   const colorScheme = useAtomValue(colorSchemeAtom)
   const color = tintColor || (colorScheme === 'dark' ? '#ffffff' : '#000000')
-  return <RefreshControl colors={[color]} tintColor={color} {...props} />
+
+  return (
+    <RefreshControl
+      progressBackgroundColor={ui.colors.base100}
+      colors={[color]}
+      tintColor={color}
+      {...props}
+    />
+  )
 }
